ebd9c9e5c9aad38876fd318198c9f740288f196b
Keith Choa <keithchoa@gmail.com>
2022-01-29 22:06:41 -0500

    Removed everything build related, changed main

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ def sigterm_handler(_signo, _stack_frame):
     # Raises SystemExit(0):
     sys.exit(0)
 
-print(os.getcwd())
-
 # ===== json parsing portion =====
 
 if not os.path.isfile("./video_logs.json"):
